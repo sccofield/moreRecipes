5c5c5400dabd0769a7c5ee02c6d6dc112794403d
sccofield <michael_umanah@yahoo.com>
2017-10-26 14:59:13 +0100

    setup node server

--- server/app.js
+++ server/app.js
@@ -1,20 +1,23 @@
 import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
+import routes from './routes';
 
-// Set up the express app
+// Set up the express app.
 const app = express();
 
-// Log requests to the console.
+// Log every requests to the console.
 app.use(logger('dev'));
 
-// Parse incoming requests data (https://github.com/expressjs/body-parser)
+// Parse incoming requests data.
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// Setup a default catch-all route that sends back a welcome message in JSON format.
-app.get('*', (req, res) => res.status(200).send({
-  message: 'Welcme to the beginning of nothingness.',
-}));
+// app.use('/', routes)
 
-module.exports = app;
+routes(app);
+
+
+// app.use('/', routes);
+
+export default app;
