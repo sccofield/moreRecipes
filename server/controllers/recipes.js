c07a67bf2dd5186e48171061741787aff6c102fe
sccofield <michael_umanah@yahoo.com>
2017-10-26 19:07:34 +0100

    add edit recipe controller

--- server/controllers/recipes.js
+++ server/controllers/recipes.js
@@ -55,6 +55,37 @@ class RecipeController {
       recipe: recipes
     });
   }
+
+  /**
+   * edit new recipe
+   * @param {object} req expres req object
+   * @param {object} res exp res object
+   * @returns {json} json
+   * @memberof RecipeController
+   */
+  editRecipe(req, res) {
+    const { id } = req.params;
+    let editRecipe;
+
+    recipes.forEach((recipe) => {
+      if (recipe.id === parseInt(id, 10)) {
+        recipe.title = req.body.title || recipe.title;
+        recipe.ingredients =
+          req.body.ingredients.split(',') || recipe.ingredients;
+        recipe.description = req.body.description || recipe.description;
+
+        editRecipe = recipe;
+      }
+    });
+    if (editRecipe) {
+      return res.status(200).json({
+        status: 'success',
+        message: 'Recipe modified',
+        recipe: editRecipe
+      });
+    }
+    return res.status(404).send(`recipe with id ${id} not found`);
+  }
 }
 
 export default RecipeController;
--- server/routes.js
+++ server/routes.js
@@ -5,6 +5,9 @@ const recipe = new Recipe.default();
 const routes = (app) => {
   // post recipe route
   app.post('/api/v1/recipes', recipe.postRecipe);
+
+  // edit recipe
+  app.put('/api/v1/recipes/:id', recipe.editRecipe);
 };
 
 export default routes;
