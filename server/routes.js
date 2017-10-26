8b62b79bd3a4abd3ec082e01a7f4ee748fcda238
sccofield <michael_umanah@yahoo.com>
2017-10-26 16:21:58 +0100

    add API route that allows a user add a recipe

--- server/routes.js
+++ server/routes.js
@@ -1,9 +1,10 @@
-
-
+import * as Recipe from './controllers/recipes';
 
 const recipe = new Recipe.default();
 
-
-
+const routes = (app) => {
+  // post recipe route
+  app.post('/api/v1/recipes', recipe.postRecipe);
+};
 
 export default routes;
