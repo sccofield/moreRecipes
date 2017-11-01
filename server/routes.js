d9c3f62a07d1386bf0083b857154e354a80ea9fa
sccofield <michael_umanah@yahoo.com>
2017-11-02 00:22:59 +0100

    ft(user-signup):add signup controller

--- server/routes.js
+++ server/routes.js
@@ -1,22 +1,36 @@
-import * as Recipe from './controllers/recipes';
+import Recipe from './controllers/recipes';
+import User from './controllers/users';
+import Middleware from './middleware';
 
-const recipe = new Recipe.default();
+// const user = new userController();
+
+// const recipe = new Recipe();
+
+// const middleware = new Middleware();
 
 const routes = (app) => {
   // post recipe route
-  app.post('/api/v1/recipes', recipe.postRecipe);
+  app.post('/api/v1/recipes', Middleware.verifyToken, Recipe.addRecipe);
 
   // edit recipe
-  app.put('/api/v1/recipes/:id', recipe.editRecipe);
+  app.put('/api/v1/recipes/:id', Middleware.verifyToken, Recipe.modifyRecipe);
 
   // delete recipe
-  app.delete('/api/v1/recipes/:id', recipe.deleteRecipe);
+  app.delete('/api/v1/recipes/:id', Middleware.verifyToken, Recipe.deleteRecipe);
 
   // Get all recipes
-  app.get('/api/v1/recipes', recipe.getAllRecipes);
+  app.get('/api/v1/recipes', Recipe.getAllRecipes);
 
   // Add review
-  app.post('/api/v1/recipes/:id/reviews', recipe.addReview)
+  app.post('/api/v1/recipes/:id/reviews', Middleware.verifyToken, Recipe.addReview);
+
+  // get favorites
+  app.get('/api/v1/users/:id/recipes', Middleware.verifyToken, Recipe.getFavorite);
+
+  app.post('/api/v1/users/signup', User.signup);
+
+  // signin route
+  app.post('/api/v1/users/signin', User.signin);
 };
 
 export default routes;
