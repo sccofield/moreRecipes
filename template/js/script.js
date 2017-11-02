fc70a05befa572f20aa625b96fb4f899fd8f9e1f
sccofield <michael_umanah@yahoo.com>
2017-11-02 23:20:04 +0100

    fix ui

--- template/js/script.js
+++ template/js/script.js
@@ -1,7 +1,6 @@
-$(document).ready(function() {
-  $(".btn-pref .btn").click(function () {
-      $(".btn-pref .btn").removeClass("btn-primary").addClass("btn-default");
-      // $(".tab").addClass("active"); // instead of this do the below 
-      $(this).removeClass("btn-default").addClass("btn-primary");   
-  });
-  });
+$(document).ready(function(){
+  $('[data-toggle="popover"]').popover(); 
+});
+$('.popover-dismiss').popover({
+  trigger: 'focus'
+})
