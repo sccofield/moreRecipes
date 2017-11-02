37a6165ead62f9d11cc2fc0f41518f81336252e7
sccofield <michael_umanah@yahoo.com>
2017-11-02 05:22:35 +0100

    ch(secure-api):add verification middleware

---
+++ server/middleware.js
@@ -0,0 +1,37 @@
+import jwt from 'jsonwebtoken';
+
+
+/**
+ * @class middleware
+ */
+class Middleware {
+  /**
+   * token check
+   * @param {object} req expres req object
+   * @param {object} res exp res object
+   * @param {object} next exp next object
+   * @returns {json} json
+   * @memberof UserController
+   */
+  static verifyToken(req, res, next) {
+    const token = req.body.token || req.query.token || req.headers.token;
+    if (token) {
+      jwt.verify(token, process.env.SECRET, (err, decoded) => {
+        if (err) {
+          return res.json({ success: false, message: 'Failed to authenticate token.' });
+        }
+        req.decoded = decoded;
+        // console.log(req.decoded);
+        // console.log(req.decoded.id);
+        next();
+      });
+    } else {
+      return res.status(403).send({
+        success: false,
+        message: 'No token provided.'
+      });
+    }
+  }
+}
+
+export default Middleware;
