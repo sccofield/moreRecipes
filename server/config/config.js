fcc8cc4dc8c363adf624d1b2824d36e34163e892
sccofield <michael_umanah@yahoo.com>
2017-11-02 19:47:06 +0100

    add process.env

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
-const { parsed: environmentVariables } = require('dotenv').config();
+// const { parsed: environmentVariables } = require('dotenv').config();
 
 const environment = process.env.NODE_ENV || 'development';
 const dialect = 'postgres';
-const url = environmentVariables.DATABASE_URL;
+const url = process.env.DATABASE_URL;
 console.log(url);
 
 
