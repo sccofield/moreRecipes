5ae5e0d677f67f4b9c8f7df86c39afeac7ad92bd
sccofield <michael_umanah@yahoo.com>
2017-12-15 12:24:49 +0100

    Ch-(implement-feedback):add update coveralls

--- server/config/config.js
+++ server/config/config.js
@@ -6,14 +6,16 @@ const config = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    dialect: 'postgres'
+    dialect: 'postgres',
+    logging: true
   },
   test: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE_TEST,
     host: process.env.DB_HOST,
-    dialect: 'postgres'
+    dialect: 'postgres',
+    logging: true
   },
   production: {
     connection_uri: process.env.DATABASE_URL,
