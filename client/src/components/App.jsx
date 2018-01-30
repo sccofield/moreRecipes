e75f42276b21d33c458c77a8db7dfd0140ad9a9d
sccofield <michael_umanah@yahoo.com>
2018-01-30 19:19:54 +0100

    ft(setup-webpack):Add webpack config for devlopment

---
+++ client/src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const App = () => (
+  <div>
+    <p>Hello the</p>
+  </div>
+);
+
+export default App;
+
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="fixed-bottom">
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <p className="text-center">
+            Copyright. All right reserve. More Recipe.
+          </p>
+        </div>
+      </div>
+
+    </div>
+  </footer>
+);
+
+export default Footer;
